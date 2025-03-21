bb38ea1ac5193fa41a9771084a394da8a25e5cb0
Donghan KIM <donghank@Donghanui-MacBookAir.local>
2025-03-22 00:14:28 +0100

    fix cookie part

--- www/cgi-bin/cookie.py
+++ www/cgi-bin/cookie.py
@@ -5,7 +5,6 @@ import cgi
 import uuid
 import time
 
-
 # directory to store session
 SESSION_DIR = "./session_data"
 
@@ -20,10 +19,13 @@ session_id = None
 
 if cookies:
     for cookie in cookies.split(';'):
-        key, value = cookie.strip().split('=', 1)
-        if key == "session_id":
-            session_id = value
-            break
+        try:
+            key, value = cookie.strip().split('=', 1)
+            if key == "session_id":
+                session_id = value
+                break
+        except ValueError:
+            continue
 
 if not session_id:
     session_id = str(uuid.uuid4())
@@ -31,10 +33,8 @@ if not session_id:
     formatted_time = time.strftime("%a, %d-%b-%Y %H:%M:%S GMT", time.gmtime(expiration_time))
     print(f"Set-Cookie: session_id={session_id}; Expires={formatted_time}; Path=/")
 
-# path session file
 session_file = os.path.join(SESSION_DIR, f"{session_id}.txt")
 
-# load visit time
 visits = 0
 try:
     with open(session_file, "r") as file:
@@ -42,10 +42,13 @@ try:
 except FileNotFoundError:
     visits = 0
 
-# increment and store
 visits += 1
 with open(session_file, "w") as file:
     file.write(str(visits))
+
+expiration_time = time.time() + 60 * 60 * 24 * 30  # 30일 유지
+formatted_time = time.strftime("%a, %d-%b-%Y %H:%M:%S GMT", time.gmtime(expiration_time))
+print(f"Set-Cookie: visits={visits}; Expires={formatted_time}; Path=/")
 
 # print HTML
 html_content = f"""
@@ -53,7 +56,7 @@ html_content = f"""
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>visit log</title>
+    <title>Visit Log</title>
     <style>
         body {{
             font-family: Arial, sans-serif;
@@ -68,8 +71,8 @@ html_content = f"""
 </head>
 <body>
     <div class="container">
-        <h1>visit log</h1>
-        <p>You're visited <strong>{visits}times</strong>.</p>
+        <h1>Visit Log</h1>
+        <p>You've visited <strong>{visits} times</strong>.</p>
     </div>
 </body>
 </html>
